f6a2fcd01f3f66db472a1a70c6e4846e15f04006
Emr <127445447+EmrD@users.noreply.github.com>
2025-02-03 21:20:30 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/Core/AccountRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Core;
-
-import com.example.demo.Models.usertable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AccountRepository extends JpaRepository<usertable, String> {} // testtable is our table name
--- demo/src/main/java/com/example/demo/Core/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Core;
-
-import com.example.demo.Models.testtable;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<testtable, String> {} // testtable is our table name
